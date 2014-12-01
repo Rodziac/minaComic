5a1f6e01b5b9e6a8625f8a40d29c3972fc81f144
umutkaya2000@gmail.com <umutkaya2000@gmail.com>
2014-12-01 20:32:10 +0000

    debugging

--- app.js
+++ app.js
@@ -2,16 +2,22 @@ var express = require('express');
 
 var app = express();
 
-app.get(/^(?!\/js|\/css|\/img).*$/, function (req, res) {
+app.use(express.static(__dirname + '/public'));
+
+app.get("/*", function(req, res){
     console.log(req);
-    res.sendFile('/public/index.html');
+    res.send("done");
 });
+//app.get(/^(?!\/js|\/css|\/img).*$/, function (req, res) {
+//    console.log(req);
+//    res.sendFile('/public/index.html');
+//});
 
 var server = app.listen(process.env.PORT || 3000, function () {
 
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log('Example app listening at http://%s:%s', host, port);
+    console.log('MinaComic listening at http://%s:%s', host, port);
 
 });

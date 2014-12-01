557a9bbf28f0e6f0d680b4630a5176fe028308e1
umutkaya2000@gmail.com <umutkaya2000@gmail.com>
2014-12-01 21:41:57 +0000

    test

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 var express = require('express');
 
 var app = express();
-//process.env.PWD = process.cwd();
 
-app.use(express.static(process.env.PWD + '/public'));
 app.get(/^(?!\/js|\/css|\/img).*$/, function (req, res) {
 
     var options = {
@@ -16,6 +14,29 @@ app.get(/^(?!\/js|\/css|\/img).*$/, function (req, res) {
     };
 
     res.sendFile('index.html', options);
+});
+
+app.get(/^(\/js|\/css|\/img).*$/, function (req, res) {
+
+    console.log("resource hit: " + req.url.substr(1));
+
+    var root = req.url.substr(1);
+    root = root.split("/");
+    var file = root[root.length - 1];
+    root = root.pop();
+    root = root.join("/");
+
+    console.log(process.env.PWD + '/public/' + root + "  :::  " + file);
+    var options = {
+        root: process.env.PWD + '/public/' + root,
+        dotfiles: 'deny',
+        headers: {
+            'x-timestamp': Date.now(),
+            'x-sent': true
+        }
+    };
+
+    res.sendFile(file, options);
 });
 
 var server = app.listen(process.env.PORT || 3000, function () {

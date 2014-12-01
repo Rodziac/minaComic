62b4f62015cb9ceddeb8af3493982274c7877e7e
umutkaya2000@gmail.com <umutkaya2000@gmail.com>
2014-12-01 20:08:03 +0000

    Route all non-resource requests to index.html

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+app.get(/^(?!\/js|\/css|img).*$/, function (req, res) {
+    res.sendFile('public/index.html');
 });
 
 var server = app.listen(process.env.PORT || 3000, function () {

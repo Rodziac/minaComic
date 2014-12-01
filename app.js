c7080edb4d6f3882d693a20f32c08fe170cdf707
umutkaya2000@gmail.com <umutkaya2000@gmail.com>
2014-12-01 21:12:26 +0000

    test

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
 app.get(/^(?!\/js|\/css|\/img).*$/, function (req, res) {
     console.log("page hit!");
 
@@ -17,37 +18,37 @@ app.get(/^(?!\/js|\/css|\/img).*$/, function (req, res) {
     res.sendFile('index.html', options);
 });
 
-app.get(/^(\/js\/closure-library).*$/, function (req, res) {
-    console.log("resource hit! " + req.url);
-
-    var options = {
-        root: __dirname + '/public/js/closure-library/',
-        dotfiles: 'deny',
-        headers: {
-            'x-timestamp': Date.now(),
-            'x-sent': true
-        }
-    };
-    var file = req.url;
-    file.replace("/js/closure-library", "");
-
-    res.sendFile(file, options);
-});
-
-app.get(/^(\/js|\/css|\/img).*$/, function (req, res) {
-    console.log("resource hit! " + req.url);
-
-    var options = {
-        root: __dirname + '/public/',
-        dotfiles: 'deny',
-        headers: {
-            'x-timestamp': Date.now(),
-            'x-sent': true
-        }
-    };
-
-    res.sendFile(req.url.substr(1), options);
-});
+//app.get(/^(\/js\/closure-library).*$/, function (req, res) {
+//    console.log("resource hit! " + req.url);
+//
+//    var options = {
+//        root: __dirname + '/public/js/closure-library/',
+//        dotfiles: 'deny',
+//        headers: {
+//            'x-timestamp': Date.now(),
+//            'x-sent': true
+//        }
+//    };
+//    var file = req.url;
+//    file.replace("/js/closure-library", "");
+//
+//    res.sendFile(file, options);
+//});
+//
+//app.get(/^(\/js|\/css|\/img).*$/, function (req, res) {
+//    console.log("resource hit! " + req.url);
+//
+//    var options = {
+//        root: __dirname + '/public/',
+//        dotfiles: 'deny',
+//        headers: {
+//            'x-timestamp': Date.now(),
+//            'x-sent': true
+//        }
+//    };
+//
+//    res.sendFile(req.url.substr(1), options);
+//});
 
 
 var server = app.listen(process.env.PORT || 3000, function () {

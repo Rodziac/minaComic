f0dacc2c9e77f8609d29e004ad9f781d16c3cd18
Umut Kaya Ilkiz <umutkaya2000@gmail.com>
2014-12-02 21:53:00 +0200

    file hosting script test

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.get(/^(?!\/js|\/css|\/img).*$/, function (req, res) {
 
     var options = {
@@ -16,27 +18,27 @@ app.get(/^(?!\/js|\/css|\/img).*$/, function (req, res) {
     res.sendFile('index.html', options);
 });
 
-app.get(/^(\/js|\/css|\/img).*$/, function (req, res) {
-
-    console.log("resource hit: " + req.url.substr(1));
-
-    var root = req.url.substr(1);
-    root = root.split("/");
-    var file = root.pop();
-    root = root.join("/");
-
-    console.log(process.env.PWD + '/public/' + root + "  :::  " + file);
-    var options = {
-        root: process.env.PWD + '/public/' + root,
-        dotfiles: 'deny',
-        headers: {
-            'x-timestamp': Date.now(),
-            'x-sent': true
-        }
-    };
-
-    res.sendFile(file, options);
-});
+//app.get(/^(\/js|\/css|\/img).*$/, function (req, res) {
+//
+//    console.log("resource hit: " + req.url.substr(1));
+//
+//    var root = req.url.substr(1);
+//    root = root.split("/");
+//    var file = root.pop();
+//    root = root.join("/");
+//
+//    console.log(process.env.PWD + '/public/' + root + "  :::  " + file);
+//    var options = {
+//        root: process.env.PWD + '/public/' + root,
+//        dotfiles: 'deny',
+//        headers: {
+//            'x-timestamp': Date.now(),
+//            'x-sent': true
+//        }
+//    };
+//
+//    res.sendFile(file, options);
+//});
 
 var server = app.listen(process.env.PORT || 3000, function () {
 
